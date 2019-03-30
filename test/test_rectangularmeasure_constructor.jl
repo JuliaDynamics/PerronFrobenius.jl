48243ffc4196bbfa34ff16522d8e99c4ea97bdd0
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-03-30 10:47:17 +0100

    Rectangular binning scheme must be specified by a RectangularBinning instance

--- test/test_rectangularmeasure_constructor.jl
+++ test/test_rectangularmeasure_constructor.jl
@@ -6,14 +6,14 @@ E = cembed([diff(rand(100)) for i = 1:4])
 @testset "RectangularInvariantMeasure constructor" begin
 
 	# On raw points
-	@test typeof(rectangularinvariantmeasure(E.points, 3)) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E.points, 0.4)) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E.points, [3, 2, 2, 3])) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E.points, [0.6, 0.5, 0.6, 0.5])) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E.points, RectangularBinning(3))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E.points, RectangularBinning(0.4))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E.points, RectangularBinning([3, 2, 2, 3]))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E.points, RectangularBinning([0.6, 0.5, 0.6, 0.5]))) <: AbstractRectangularInvariantMeasure
 
 	# On embeddings
-	@test typeof(rectangularinvariantmeasure(E, 3)) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E, 0.4)) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E, [3, 2, 2, 3])) <: AbstractRectangularInvariantMeasure
-	@test typeof(rectangularinvariantmeasure(E, [0.6, 0.5, 0.6, 0.5])) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E, RectangularBinning(3))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E, RectangularBinning(0.4))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E, RectangularBinning([3, 2, 2, 3]))) <: AbstractRectangularInvariantMeasure
+	@test typeof(rectangularinvariantmeasure(E, RectangularBinning([0.6, 0.5, 0.6, 0.5]))) <: AbstractRectangularInvariantMeasure
 end
