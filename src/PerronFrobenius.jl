38a8b223cf63d56eff7ab34452ade36b2766c3ba
kristian <kahaaga@gmail.com>
2020-06-17 14:53:27 +0200

    Fix bad path

--- src/PerronFrobenius.jl
+++ src/PerronFrobenius.jl
@@ -13,7 +13,7 @@ module PerronFrobenius
     using Requires 
     function __init__()
         @require Simplices="d5428e67-3037-59ba-9ab1-57a04f0a3b6a" begin
-            include("v1/triangulationestimators/TriangulationEstimators.jl")
+            include("triangulationestimators/TriangulationEstimators.jl")
         end
 
         @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
